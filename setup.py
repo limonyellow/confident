17e67467d915f71144d127460443c66e44ac641d
limonada <62614138+limonyellow@users.noreply.github.com>
2022-04-01 14:56:06 +0300

    Update python Versions to 3.7-3.10 (#28)
    * Update python Versions to 3.7-3.10
    
    * Update python Versions to 3.7-3.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
       name='confident',
-      version='0.1.9',
+      version='0.1.10',
       description='Loading configurations from multiple sources into a data model.',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
@@ -13,14 +13,18 @@ setuptools.setup(
       zip_safe=False,
       classifiers=[
             "Programming Language :: Python :: 3",
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
       ],
       packages=['confident'],
       install_requires=[
-            'pydantic',
-            'pyyaml',
-            'python-dotenv',
+            'pydantic>=1.9.0',
+            'pyyaml>=6.0',
+            'python-dotenv>=0.10.4',
       ],
-      python_requires=">=3.6",
+      python_requires=">=3.7",
 )
