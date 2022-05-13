8ebfbbb71a717aefbeeba4efbadac3bd8dfa51ad
limonada <62614138+limonyellow@users.noreply.github.com>
2022-05-13 16:05:26 +0300

    Use temporary context between the model and the Config class (#41)

--- confident/confident.py
+++ confident/confident.py
@@ -17,39 +17,53 @@ from confident.loaders.map_source_loader import MapSourceLoader
 from confident.specs import SettingsSpecs
 from confident.utils import get_class_file_path
 
+SPECS_ATTR = '_specs'
+LOADER_MANAGER_ATTR = '_loader_manager'
+
 
 class Confident(BaseSettings):
+    __slots__ = (SPECS_ATTR, LOADER_MANAGER_ATTR)
+
     def __init__(self, **values: Any):
         # Prepare metadata.
         subclass_location = get_class_file_path(cls=self)
         caller_module = inspect.getmodule(inspect.stack()[1][0])
         caller_location = caller_module.__file__ if caller_module else Path.cwd()
 
-        if not self.__config__._specs:
+        specs = self.Config.__dict__.get('specs')
+        if not specs:
             specs_path = values.pop('_specs_path', None) or self.Config.__dict__.get('specs_path')
             if specs_path:
-                self.__config__._specs = SettingsSpecs.from_path(
+                specs = SettingsSpecs.from_path(
                     path=specs_path, class_path=subclass_location, creation_path=caller_location
                 )
             else:
-                self.__config__._specs = SettingsSpecs.from_model(
+                specs = SettingsSpecs.from_model(
                     model=self, values=values, class_path=subclass_location, creation_path=caller_location
                 )
 
-        self.__config__._loader_manager = LoaderManager(
-            settings_obj=self, source_priority=self.__config__._specs.source_priority
-        )
+        loader_manager = LoaderManager(settings_obj=self, source_priority=specs.source_priority)
+        object.__setattr__(self, SPECS_ATTR, specs)
+        object.__setattr__(self, LOADER_MANAGER_ATTR, loader_manager)
+
+        # Create temporary context for the scope of `Config` class.
+        self.Config._confident_loader_manager_context_ = loader_manager
+        self.Config._confident_specs_context_ = specs
+
         super().__init__(**values)
+
+        # Reset the context of the Config class, so it will be clean for the next object creation.
+        del self.Config._confident_loader_manager_context_
+        del self.Config._confident_specs_context_
 
     def _build_values(self, init_kwargs: Dict[str, Any], **kwargs) -> Dict[str, Any]:
         values_dict = super()._build_values(init_kwargs, **kwargs)
-        self.__config__._loader_manager.full_fields = values_dict
+        loader_manager = self.__getattribute__(LOADER_MANAGER_ATTR)
+        loader_manager.full_fields = values_dict
+
         return {name: field.value for name, field in values_dict.items()}
 
     class Config:
-        _loader_manager = None
-        _specs = None
-
         @classmethod
         def customise_sources(
             cls,
@@ -58,41 +72,43 @@ class Confident(BaseSettings):
             file_secret_settings: SettingsSourceCallable,
         ) -> Tuple[SettingsSourceCallable, ...]:
             # Get BaseSettings default loaders callables.
-            cls._loader_manager.init_settings_callable = init_settings
-            cls._loader_manager.env_settings_callable = env_settings
-            cls._loader_manager.file_secret_settings_callable = file_secret_settings
+            loader_manager = cls._confident_loader_manager_context_
+            specs = cls._confident_specs_context_
+            loader_manager.init_settings_callable = init_settings
+            loader_manager.env_settings_callable = env_settings
+            loader_manager.file_secret_settings_callable = file_secret_settings
 
             # Add all the default loaders.
-            cls._loader_manager.loaders.extend([
-                InitSourceLoader(specs=cls._specs, init_settings_callable=init_settings),
-                EnvSourceLoader(specs=cls._specs, env_settings_callable=env_settings),
-                MapSourceLoader(specs=cls._specs, all_loaded_fields=cls._loader_manager.all_loaded_fields),
-                FileSourceLoader(specs=cls._specs),
-                DefaultSourceLoader(specs=cls._specs)
+            loader_manager.loaders.extend([
+                InitSourceLoader(specs=specs, init_settings_callable=init_settings),
+                EnvSourceLoader(specs=specs, env_settings_callable=env_settings),
+                MapSourceLoader(specs=specs, all_loaded_fields=loader_manager.all_loaded_fields),
+                FileSourceLoader(specs=specs),
+                DefaultSourceLoader(specs=specs)
             ])
 
-            return cls._loader_manager.load_all()
+            return loader_manager.load_all()
 
     def specs(self) -> SettingsSpecs:
         """
         Returns: A deep copy of the settings class specs.
         """
-        return deepcopy(self.__config__._specs)
-
-    def full_fields(self) -> Dict[str, Any]:
-        """
-        Returns: A dictionary with details of every field.
-        """
-        return deepcopy(self.__config__._loader_manager.full_fields)
-
-    def all_loaded_fields(self) -> Dict[str, List[ConfigProperty]]:
-        """
-        Returns: A dictionary with details of every field.
-        """
-        return deepcopy(self.__config__._loader_manager.all_loaded_fields)
+        return deepcopy(self.__getattribute__(SPECS_ATTR))
 
     def source_priority(self) -> List[ConfigSource]:
         """
         Returns: List of sources from the highest priority to the lowest.
         """
-        return deepcopy(self.__config__._specs.source_priority)
+        return deepcopy(self.__getattribute__(SPECS_ATTR).source_priority)
+
+    def full_fields(self) -> Dict[str, Any]:
+        """
+        Returns: A dictionary with details of every field.
+        """
+        return deepcopy(self.__getattribute__(LOADER_MANAGER_ATTR).full_fields)
+
+    def all_loaded_fields(self) -> Dict[str, List[ConfigProperty]]:
+        """
+        Returns: A dictionary with details of every field.
+        """
+        return deepcopy(self.__getattribute__(LOADER_MANAGER_ATTR).all_loaded_fields)
